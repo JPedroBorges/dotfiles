f97657039680e514fb12157efd393745df96a8ea
JPedroBorges <JPedroBorges@users.noreply.github.com>
2023-10-30 19:30:42 +0000

    add current macos configuration

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+[ -z $1 ] && echo "Missing passphrase" && exit 1
+
 # Ask for the administrator password upfront
 sudo -v
 
@@ -49,3 +51,28 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "configuring ssh"
+ssh-keygen -t ed25519 -C "joao.borges@fanduel.com" -N "$1" -f ~/.ssh/github_id_ed25519
+
+echo "cloning dotfiles"
+git clone https://github.com/JPedroBorges/dotfiles.git "${HOME}/.dotfiles"
+ln -s "${HOME}/.dotfiles/joao.borges/.zshrc" "${HOME}/.zshrc"
+ln -s "${HOME}/.dotfiles/joao.borges/.p10k.zsh" "${HOME}/.p10k.zsh"
+ln -s "${HOME}/.dotfiles/joao.borges/.gitconfig" "${HOME}/.gitconfig"
+ln -s "${HOME}/.dotfiles/joao.borges/Library/Application Support/Code/User/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
+ln -s "${HOME}/.dotfiles/joao.borges/Library/DBeaverData/workspace6/General/.dbeaver/data-sources.json" "${HOME}/Library/DBeaverData/workspace6/General/.dbeaver/data-sources.json"
+ln -s "${HOME}/.dotfiles/joao.borges/.ssh/config" "${HOME}/.ssh/config"
+ln -s "${HOME}/.dotfiles/joao.borges/Library/Application Support/iTerm2/DynamicProfiles/joao.borges.json" "${HOME}/Library/Application Support/iTerm2/DynamicProfiles/joao.borges.json"
+
+printf "TODO:\n\
+\tconfigure: \n\
+\t\tGitHub: SSH Key (pbcopy < ~/.ssh/github_id_ed25519.pub) \n\
+\t\tIntellij: File | Manage IDE Settings | Import Settings | intellij/settings.zip \n\
+\t\tCertificates: https://fanduel.atlassian.net/wiki/spaces/~674092252/pages/308064945548/macos+starter+guide
+\n
+\tinstall: \n\
+\t\tAmphetamine (https://apps.apple.com/pt/app/amphetamine/id937984704?mt=12)  \n\
+\t\tClipy (https://clipy-app.com)  \n\
+\t\tMaven@3.6.3 (https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.6.3) to \"/opt/apache-maven\"  \n\
+"
